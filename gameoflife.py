b3ddbf23b7419c3a22dbbbbdcc79bd29eda03ec9
Manuel Sabin <msabin27@gmail.com>
2023-08-08 10:46:59 -0700

    Implemented check for the first rule of Game of Life. Not sure if it works perfect yet, but fixed some index errors and this loop should be able to do all the countin we need to simply check for the other rules.

--- gameoflife.py
+++ gameoflife.py
@@ -3,9 +3,12 @@ o='O' # alive
 
 grid=[]
 
-for i in range(10):
+m = 11 # num rows
+n = 10 # num cols
+
+for i in range(m):
     a = []
-    for j in range(10):
+    for j in range(n):
         a.append(x)
     grid.append(a)
 
@@ -13,4 +16,41 @@ grid[5][5] = o
 grid[4][5] = o
 grid[4][6] = o
 print(grid)
+
+
+# Make the rules
+iterations = 1
+for k in range(iterations):
+
+    for i, row in enumerate(grid):
+
+        for j, cell in enumerate(row):
+            # Any live cell with two or three live neighbours survives.
+
+            livingNeighbors = 0
+            for horizontal in range(3):
+                horizontal += -1
+                
+
+                if horizontal + j < 0 or horizontal + j > n-1:
+                    continue
+
+                for vertical in range(3):
+                    # Make them be in {-1, 0, 1}
+                    vertical += -1
+
+
+                    if vertical + i < 0 or vertical + i > m-1:
+                        continue
+                    
+                    if vertical + i == 0 and horizontal + j == 0:
+                        continue
+
+                    if grid[i + vertical][j + horizontal] == o:
+                        livingNeighbors += 1
+
+            print("row: ", i, "col: ", j, "neighbors: ", livingNeighbors)
+
+                    
+
 
